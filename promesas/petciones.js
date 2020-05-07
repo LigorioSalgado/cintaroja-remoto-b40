aef2f15d098439d7c3ebb884c92c2baee8ecf558
LigorioSalgado <salgadolg5@hotmail.com>
2020-05-06 22:01:01 -0500

    promises cool

---
+++ promesas/petciones.js
@@ -0,0 +1,85 @@
+const axios = require('axios');
+
+function getCharacter(id){
+ const url = `https://swapi.dev/api/people/${id}`
+
+
+ const character = axios.get(url)
+
+ character.then((response) => {
+     console.log("====== en then ======")
+     console.log(response.data)
+     console.log(response.status)
+ })
+ .catch((error) => {
+    console.log("====== en catch ======")
+    console.log(error.response.data)
+    console.log(error.response.status)
+ })
+
+
+// axios.get(url).catch((error) =>{
+//     console.log("====== en catch ======")
+//     console.log(error.response.data)
+//     console.log(error.response.status)
+// })
+// .then((response) => {
+//     console.log("====== en then ======")
+//     console.log(response.data)
+//     console.log(response.status)
+   
+// })
+
+
+
+ console.log(character)
+
+}
+
+//getCharacter(13)
+
+function getProfile(id){
+    const url = `https://swapi.dev/api/people/${id}`
+
+    const character = axios.get(url)
+
+    character.then((response) => {
+        return response.data //esto esta regresando otra promesa estoy regresando el personaje
+    }).then((personaje) => {
+        return axios.get(personaje.homeworld).then((response) =>{
+            return "desde dentro de la promesa" //split object es6 crea una copia de un objeto
+        })  
+    }).then((personaje) => {
+        console.log(per)
+        console.log("El personaje que escogiste es: ", personaje.name)
+        console.log("Nacio en el año: ", personaje.birth_year)
+        console.log("Tiene el genero: ", personaje.gender)
+        console.log("Su planeta es: ", personaje.homeworld.name)
+        return personaje
+    }).then((elmismopersonaje) => {
+        console.log(elmismopersonaje)
+    }).catch((error) => {
+        console.log(error.response.data)
+    })
+    
+
+
+    // character.then((response) => {
+    //     const personaje = response.data
+    //     console.log("El personaje que escogiste es: ", personaje.name)
+    //     console.log("Nacio en el año: ", personaje.birth_year)
+    //     console.log("Tiene el genero: ", personaje.gender)
+    //     axios.get(personaje.homeworld).then((response) => {
+    //         const planeta = response.data
+    //         console.log("Su planeta es: ", planeta.name)
+    //     }).catch((error) => {
+    //         console.log(error.response.data)
+    //     })
+    // }) 
+    // .catch((error) => {
+    //     console.log(error.response.data)
+    // })
+
+}
+
+getProfile(5)
