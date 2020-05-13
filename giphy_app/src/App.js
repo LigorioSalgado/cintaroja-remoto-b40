61294f238e437075c413242e0962b059af8cc740
LigorioSalgado <salgadolg5@hotmail.com>
2020-05-12 21:41:48 -0500

    berfore card component

--- giphy_app/src/App.js
+++ giphy_app/src/App.js
@@ -3,17 +3,29 @@ import './App.css';
 import axios from 'axios'; 
 
 function App() {
-  const [search,setSearch] = useState('') //Este va a guardar el valor a buscar de los gifs
-  const [gifs, setGifs] = useState(''); //Esto va a guardar los gifs de la api
+  const URL = 'https://api.giphy.com/v1/gifs/search?api_key=pqpMKMEFrBUb427UCUNV2tuAffbHsExy&q='
+  const [search,setSearch] = useState('hello') //Este va a guardar el valor a buscar de los gifs
+  const [gifs, setGifs] = useState([]); //Esto va a guardar los gifs de la api
 
   useEffect(() => {
     console.log('Desde use effect')
-    console.log(search)
+
+    axios.get(URL+search).then((response) => {
+      setGifs(response.data.data) //primer data es de axios, segundo data es de respuesta de giphy
+    }).catch((error) => {
+      console.log(error)
+    })
+
   },[])
 
   const findGif = (event) => {
     event.preventDefault() //Evita que recargue la pagina al momento de hacer submit al form
     //llamada a la api
+    axios.get(URL+search).then((response) => {
+      setGifs(response.data.data) //primer data es de axios, segundo data es de respuesta de giphy
+    }).catch((error) => {
+      console.log(error)
+    })
 
   }
 
@@ -26,31 +38,29 @@ function App() {
               <form onSubmit={findGif} className="form-inline justify-content-center">
                   <input className="form-control" 
                     onChange={(event) => setSearch(event.target.value) } 
-                    placeholder="Ej. gatitos" name="gif"  
+                    placeholder={`Ej. ${search}`} name="gif" 
                   />
                   <button type="submit" className="btn  btn-success">Buscar</button>
               </form>
             </div>
         </div>
         <div className="row mt-5">
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-              <div className="card bg-dark text-white">
-                  <img className="card-img" alt="" src="https://media.giphy.com/media/slVWEctHZKvWU/giphy.gif" />
-                  <h5 className="card-title">Pikachu en piscina</h5>
+          { gifs.length > 0 ? gifs.map((gif) => {
+            return(
+              <div className="col-12 col-sm-12 col-md-4 col-lg-4 card-deck my-5">
+                <div className="card bg-dark text-white">
+                    <img className="card-img" alt="" src={gif.images.downsized_large.url} />
+                    <h5 className="card-title">{gif.title}</h5>
+                </div>
               </div>
-          </div>
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-              <div className="card bg-dark text-white">
-                  <img className="card-img" alt="" src="https://media.giphy.com/media/slVWEctHZKvWU/giphy.gif" />
-                  <h5 className="card-title">Pikachu en piscina</h5>
-              </div>
-          </div>
-          <div className="col-12 col-sm-12 col-md-4 col-lg-4">
-              <div className="card bg-dark text-white">
-                  <img className="card-img" alt="" src="https://media.giphy.com/media/slVWEctHZKvWU/giphy.gif" />
-                  <h5 className="card-title">Pikachu en piscina</h5>
-              </div>
-          </div>
+            )
+          }) : (
+            <div className="col-12">
+                <h1>No hay resultados en tu busqueda 😪</h1>
+            </div>
+          )
+        
+        }
         </div>
       </div>
     </div>
