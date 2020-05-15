56a2d6a97363f458908d7242a67aeaa978dc657c
LigorioSalgado <salgadolg5@hotmail.com>
2020-05-14 19:44:12 -0500

    home and card

---
+++ todo_app/src/components/CardTodo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function CardTodo(props){
+
+    const bg ={
+        "1":"card text-white bg-success",
+        "2":"card text-white bg-warning",
+        "3":"card text-white bg-danger"
+    }
+
+    return(
+        <div className={bg[props.prioridad]}>
+            <div className="card-header">  Creado por:{props.nombre}</div>
+            <div className="card-body">
+                <p className="card-text">{props.todo}</p>
+            </div>
+        </div>
+    )
+}
---
+++ todo_app/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+
+
+function Home(){
+
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-12 col-sm-8 col-md-8 col-lg-8">
+
+                </div>
+            </div>
+        </div>
+    )
+
+}
